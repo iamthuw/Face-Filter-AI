218f6628b1703dd4ff431ccac1f3f16071fd0454
NgThach <167107938+NgThach@users.noreply.github.com>
2025-11-05 07:28:05 +0700

    Delete src/preprocessing directory

--- src/preprocessing/split_dataset.py
+++
@@ -1,5 +0,0 @@
-from sklearn.model_selection import train_test_split
-
-def split_dataset(all_data, test_size=0.2, seed=42):
-    """Chia dataset thành train và test."""
-    return train_test_split(all_data, test_size=test_size, random_state=seed)
